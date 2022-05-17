7971b6ee0c8e5ded408dfff840b8fe32a669d85e
gingercook <vahitaras@gmail.com>
2022-05-16 22:18:03 -0400

    end of the basics

--- basics.js
+++ basics.js
@@ -2,105 +2,86 @@
 // Complex types: array, object
 // Function types, parameters
 ////////////////////////////////////////////////
-
-
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
 // Primitives: number, string, boolean
-let age: number;
+var age;
 age = 12;
-let age2: number = 20;
-
-let userName: string;
+var age2 = 20;
+var userName;
 userName = 'Aras';
-
-let isDeveloper: boolean;
+var isDeveloper;
 isDeveloper = true;
-
 // Complex types: array, object
-let hobbies: string[];
+var hobbies;
 hobbies = ['Programming', 'Chess'];
-
 // Alternatively it can be written like that.
-const hobbies2: Array<string> = ['Programming', 'Chess'];
-
-
-let person: any;
+var hobbies2 = ['Programming', 'Chess'];
+var person;
 person = {
     name: 'James',
     age: 30
-}
-
-let person2;
+};
+var person2;
 person2 = {
     name: 'Michael',
     age: 18
-}
-
-let person3: {
-    name: string;
-    age: number;
-}
+};
+var person3;
 person3 = {
     name: 'Davis',
     age: 47
-}
-
-let personArray: {
-    name: string;
-    age: number;
-}[]
+};
+var personArray;
 personArray = [
     {
         name: 'Laura',
         age: 10
     },
-
     {
         name: 'Daniel',
         age: 45
     },
 ];
-
 // Type inference
-let course = 'React';
+var course = 'React';
 // TypeScript will assume that the variable
 // is a String, so you cannot change it to a
 // number. This is called "Type inference".
 // course = 123;
-
-let unionType: string | number | boolean = 'String value is hold in this var!';
+var unionType = 'String value is hold in this var!';
 unionType = false;
-
-let arrayOfStrings: string[] = ['Aras', 'Sen'];
-
-type typeAlias = {
-    occupation: string;
-    experience: number
-};
-
-let newDude: typeAlias;
-let lottaDudes: typeAlias[];
-
+var arrayOfStrings = ['Aras', 'Sen'];
+var newDude;
+var lottaDudes;
 // Function types, parameters
-function add(a: number, b: number): number | string {
+function add(a, b) {
     return a + b;
-};
-
+}
+;
 // it has a void return type "function printOutput(value: any): void"
 // if you hover over it on VS Code. It means that this function
 // never returns anything.
-function printOutput(value: any) {
+function printOutput(value) {
     console.log(value);
-};
-
+}
+;
 // Generics
 // <T> is for generic type only available in this function
 // T stands for the fact that all arrays and return values
 // should be having the same type. It helps to infer the values
-function insertAtBeginning<T>(array: T[], value: T) {
-    const newArray = [value, ...array];
+function insertAtBeginning(array, value) {
+    var newArray = __spreadArray([value], array, true);
     return newArray;
-};
-
-const demoArray = ['1', '2', '3'];
-const updatedArray = insertAtBeginning(demoArray, '5');
+}
+;
+var demoArray = ['1', '2', '3'];
+var updatedArray = insertAtBeginning(demoArray, '5');
 console.log(updatedArray);
--- with-typescript.ts
+++
@@ -1,5 +0,0 @@
-function add(a: number, b: number) {
-    return a + b;
-}
-var result = add(2, 5);
-console.log(result);
